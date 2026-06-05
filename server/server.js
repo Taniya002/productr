271fa9527d251f84584f93e799488b6b5dafe964
Taniya Tondwal <taniya8687@gmail.com>
2026-06-05 14:07:59 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const { errorMiddleware, notFound } = require("./middleware/error.middleware");
 connectDB();
 
 const app = express();
-
+app.set("trust proxy", 1);
 app.use(helmet());
 
 app.use(
--- server/utils/otp.utils.js
+++ server/utils/otp.utils.js
@@ -7,14 +7,14 @@ const generateOTP = () => {
 const createTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: parseInt(process.env.EMAIL_PORT),
-    secure: false,
+    port: 465,        
+    secure: true,     
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
-};
+}
 
 const sendOTPEmail = async (email, otp) => {
   const transporter = createTransporter();
